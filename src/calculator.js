d7e585e315e3d61f367e380c4be6621c90f95ea8
Ekram-ekiti <ekru6482@gmail.com>
2026-05-21 11:45:51 +0300

    your message

--- src/calculator.js
+++ src/calculator.js
@@ -1,5 +1,5 @@
 function add(a, b) {
-  return a + b;
+  return b + b;
 }
 
 function subtract(a, b) {
@@ -7,7 +7,7 @@ function subtract(a, b) {
 }
 
 function multiply(a, b) {
-  return a * b;
+  return b * b;
 }
 
 function divide(a, b) {
@@ -21,5 +21,5 @@ module.exports = {
   add,
   subtract,
   multiply,
-  divide
+  divide,
 };
